6949ea2e97abb251bb797a9659307b6f3755c1d9
Tejaswini2021172 <tej.v3105@gmail.com>
2026-09-05 17:53:46 +0530

    Add files via upload

---
+++ LAB/Lab programs-1/EXP-6_192472088.py
@@ -0,0 +1,11 @@
+import torch
+
+a = torch.tensor([2,4,6])
+b = torch.tensor([1,3,5])
+
+print("Tensor A:", a)
+print("Tensor B:", b)
+
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
